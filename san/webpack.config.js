97ea1d26798be85dc874f70d27eb5276231c7f68
kouchendi <koucxz@hotmail.com>
2018-04-30 23:15:55 +0800

    add webpack plugins

--- san/webpack.config.js
+++ san/webpack.config.js
@@ -1,41 +1,54 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: './',
-    port: 8080
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }, 
-      {
-        test: /\.san$/,
-        use: [
-          'san-loader'
-        ]
-      }
-    ]
+module.exports = function(env)  {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    devtool: env.production ? '' : 'inline-source-map',
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'san'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, "src"),
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/transform-runtime']
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        }, 
+        {
+          test: /\.san$/,
+          use: [
+            'san-loader'
+          ]
+        }
+      ]
+    }
   }
 }
