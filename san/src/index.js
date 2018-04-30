b822f76a45fcdd1f95d8ca9fb2648b473638f2c7
kouchendi <koucxz@hotmail.com>
2018-05-01 00:40:50 +0800

    (mvvm 2.1)page data bind

--- san/src/index.js
+++ san/src/index.js
@@ -7,11 +7,28 @@ class HelloComponent extends Component {
     }
 
     static template = `
-      <p>Hello {{name}}!</p>
+      <div>
+        <form>
+          <input type="text" value="{= name =}" placeholder="姓名(string)">
+          <input type="number" value="{= age =}" placeholder="年龄(number)">
+          <input type="text" value="{= des =}" placeholder="简介(string)">
+        </form>
+        <div>
+          信息: <button>移除信息</button>
+        </div>
+        <!-- 数据空时显示下划线 -->
+        <p>姓名：{{name || '_________'}}</p>
+        <p>年龄：{{age || '_________'}}</p>
+        <p>简介：{{des || '_________'}}</p>
+      </div>
     `;
 
     initData() {
-        return {name: 'San'}
+        return {
+          name: '',
+          age: '',
+          des: ''
+        }
     }
 }
 
