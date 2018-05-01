77f76460e6682427e65f25c691fe47d959f25e32
kouchendi <koucxz@hotmail.com>
2018-05-01 22:32:29 +0800

    2.2 s-if and s-for

--- san/src/index.js
+++ san/src/index.js
@@ -1,8 +1,12 @@
+import './main.css'
 // route
-import App from './pages/App.san'
+import DataBind from './pages/DataBind.san'
+import IfFor from './pages/IfFor.san'
 
 import {router} from 'san-router'
 
-router.add({rule: '/', Component: App, target: '#app'})
+router.add({rule: '/', Component: IfFor, target: '#app'})
+router.add({rule: '/bind', Component: DataBind, target: '#app'})
+router.add({rule: '/if', Component: IfFor, target: '#app'})
 // start
 router.start()
