a826090f75f5ea397802a27bbd6cb5a0d02de12b
kouchendi <koucxz@hotmail.com>
2018-05-06 22:10:57 +0800

    2.3 style control

---
+++ san/README.md
@@ -0,0 +1,8 @@
+# Install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production
+npm run build
--- san/src/index.js
+++ san/src/index.js
@@ -2,11 +2,13 @@ import './main.css'
 // route
 import DataBind from './pages/DataBind.san'
 import IfFor from './pages/IfFor.san'
+import styleCtr from './pages/styleCtr.san'
 
 import {router} from 'san-router'
 
-router.add({rule: '/', Component: IfFor, target: '#app'})
+router.add({rule: '/', Component: styleCtr, target: '#app'})
 router.add({rule: '/bind', Component: DataBind, target: '#app'})
 router.add({rule: '/if', Component: IfFor, target: '#app'})
+router.add({rule: '/style', Component: styleCtr, target: '#app'})
 // start
 router.start()
