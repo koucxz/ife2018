8f98697a7cae20da0a1ab8f6ff2193e109464fc8
kouchendi <koucxz@hotmail.com>
2018-05-12 16:25:32 +0800

    2.4 事件处理

--- README.md
+++ README.md
@@ -24,3 +24,10 @@
 
 [2.3](https://github.com/koucxz/ife2018/tree/2.3/san/src/pages/styleCtr.san)
 - 样式控制
+
+[2.4](https://github.com/koucxz/ife2018/tree/2.4/san/src/pages/EventEmit.san)
+- 事件处理
+1.父组件与高层父组件显示默认值
+2.表单获得焦点输入自定义的信息，点击通知父组件按钮，父组件以及更高层父组件显示输入框内容
+3.父组件及高层父组件接收到子组件的信息时，文案样式发生变化
+4.父组件文案可编辑并能同时控制子组件输入框文案与父组件保持一致
--- san/src/index.js
+++ san/src/index.js
@@ -2,13 +2,15 @@ import './main.css'
 // route
 import DataBind from './pages/DataBind.san'
 import IfFor from './pages/IfFor.san'
-import styleCtr from './pages/styleCtr.san'
+import StyleCtr from './pages/StyleCtr.san'
+import EventEmit from './pages/EventEmit.san'
 
 import {router} from 'san-router'
 
-router.add({rule: '/', Component: styleCtr, target: '#app'})
+router.add({rule: '/', Component: EventEmit, target: '#app'})
 router.add({rule: '/bind', Component: DataBind, target: '#app'})
 router.add({rule: '/if', Component: IfFor, target: '#app'})
-router.add({rule: '/style', Component: styleCtr, target: '#app'})
+router.add({rule: '/style', Component: StyleCtr, target: '#app'})
+router.add({rule: '/event', Component: EventEmit, target: '#app'})
 // start
 router.start()
